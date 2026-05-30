c6413787f68cd6f9b9b6d3833b92c9043bc84221
dragon731012 <dragon73101@gmail.com>
2026-05-30 11:47:29 -0500

    particles

--- animations.js
+++ animations.js
@@ -80,3 +80,57 @@ function startBounce(e){
     }
     bounce(800);
 }
+
+function randomnum(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+//particle stuff
+let canvas=document.createElement("canvas");
+let ctx=canvas.getContext('2d');
+let rad=1;
+let a=50;
+canvas.style="position:fixed;top:0px;left:0px;z-index:-3;";
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+document.body.appendChild(canvas);
+
+let particles=[];
+
+for (let i=0;i<a;i++){
+    particles.push({
+        x: randomnum(0,window.innerWidth),
+        y: randomnum(0,window.innerHeight),
+        vx: (Math.random() - 0.5) * 0.5,
+        vy: (Math.random() - 0.5) * 0.5,
+        c: (i<a/2)?"b":"r"
+    });
+}
+
+function animationloop(){
+    ctx.clearRect(0, 0, window.innerWidth, window.innerHeight);
+
+    for (let p of particles){
+        ctx.beginPath();
+        ctx.arc(p.x, p.y, rad, 0, 2 * Math.PI);
+        p.x+=p.vx;
+        p.y+=p.vy;
+        if (p.x<0) p.x=window.innerWidth;
+        if (p.x>window.innerWidth) p.x=0;
+        if (p.y<0) p.y=window.innerHeight;
+        if (p.y>window.innerHeight) p.y=0;
+    
+        if (p.c=="r"){
+            ctx.strokeStyle = "#ff4538";
+            ctx.fillStyle = "#ff8178";
+        } else {
+            ctx.strokeStyle = '#007bff';
+            ctx.fillStyle = '#abcfff';
+        }
+
+        ctx.stroke();
+        ctx.fill();
+    }
+    
+    requestAnimationFrame(animationloop);
+}
